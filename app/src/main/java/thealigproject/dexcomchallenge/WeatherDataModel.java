fa60a2bc6fa01921a504b86dd0d139ff1b578533
Ali <aaghaffa@gmail.com>
2018-01-15 22:14:10 -0800

    Project Complete.

--- app/src/main/java/thealigproject/dexcomchallenge/WeatherDataModel.java
+++ app/src/main/java/thealigproject/dexcomchallenge/WeatherDataModel.java
@@ -50,7 +50,7 @@ public class WeatherDataModel {
 
             WeatherDataModel weatherData = new WeatherDataModel();
 
-            //Condition for each day of the week
+            //Condition (in icon form) for each day of the week
             weatherData.mCondition = jsonObject.getJSONObject("daily").getJSONArray("data")
                     .getJSONObject(0).getString("icon");
             weatherData.mIconName = updateWeatherIcon(weatherData.mCondition);
